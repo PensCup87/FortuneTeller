f657f48e0728c5f09c9971958047976c5cc1c697
PensCup87 <bdt9123@gmail.com>
2017-09-29 11:21:28 -0400

    Initial Commit Fortune Teller Project

---
+++ FortuneTellerProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortuneTellerProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cold Open
+            Console.WriteLine("\aGreetings from The All Known.\nYou want some insight and I'll need some details.\nIf unsatisfied upon completion, customer service will instruct you to try again and lie.");
+            
+
+        }
+    }
+}
