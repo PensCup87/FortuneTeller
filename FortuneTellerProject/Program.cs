7de2df303b4f9a335b2bbe90ede96745b4514800
PensCup87 <bdt9123@gmail.com>
2017-09-29 11:56:26 -0400

    Part 1 complete

--- FortuneTellerProject/Program.cs
+++ FortuneTellerProject/Program.cs
@@ -11,8 +11,29 @@ namespace FortuneTellerProject
         static void Main(string[] args)
         {
             //Cold Open
-            Console.WriteLine("\aGreetings from The All Known.\nYou want some insight and I'll need some details.\nIf unsatisfied upon completion, customer service will instruct you to try again and lie.");
-            
+            Console.WriteLine("Greetings from The All Knowing.\n\nYou want some insight and I'll need some details.\n***Disclaimer***\nIf unsatisfied upon completion, customer service will instruct you to try again and lie.");
+
+            //Gather Personal Details
+            Console.WriteLine("\n\nRather than referring to you as Baba Booey, Please enter your First Name:");
+            string userFirst = Console.ReadLine().Trim().ToLower();
+
+            Console.WriteLine("\nPlease enter your Last Name:");
+            string userLast = Console.ReadLine().Trim().ToLower();
+
+            Console.WriteLine("\nPlease enter your age:");
+            int userAge = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPlease enter the number representing your birth month:");
+            int birthMonth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nWhat ROYGBIV is your favorite:\nIf you do not know ROYGBIV, enter \"Help\"");
+            string favoriteColor = Console.ReadLine().ToLower().Trim();
+
+            Console.WriteLine("\nPlease enter the number of siblings you have:");
+            int userSiblings = int.Parse(Console.ReadLine());
+
+
+
 
         }
     }
